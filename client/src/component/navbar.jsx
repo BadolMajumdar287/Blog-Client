8147f1f013dfadf3a6306c40f089a91cc7406cee
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-08-01 16:33:53 +0530

    add

--- client/src/component/navbar.jsx
+++ client/src/component/navbar.jsx
@@ -8,7 +8,6 @@ export default function Navbar() {
   return (
     <div className="fixed top-0 right-0 left-0 h-9 w-full flex justify-center gap-2">
         <button className="border w-fit h-fit rounded-2xl bg-cyan-950 text-cyan-400" onClick={() => router.push("/blog")}>Blog Page</button>
-
         <button className="border w-fit h-fit rounded-2xl text-cyan-400 bg-cyan-950" onClick={() => router.push("/admin/login")}>Admin Login</button>
       </div>
    
