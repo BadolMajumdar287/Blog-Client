cfacd8575f05f9c3e2427cca85e549510234103a
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-08-02 21:16:03 +0530

    add baseUrl

--- client/src/config/api.config.jsx
+++ client/src/config/api.config.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const ApiManager = axios.create({
-   baseURL: process.env.NEXT_PUBLIC_API_BASE,
-    //baseURL:"/api",
+   //baseURL: process.env.NEXT_PUBLIC_API_BASE,
+    baseURL:"/api",
     withCredentials: true,
 
 });
