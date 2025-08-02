8961ec667c796445c6eaa6fcf114ab9db04a7438
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-08-02 13:05:49 +0530

    delete dev origin

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -10,9 +10,7 @@ const nextConfig = {
         ];
     },
 
-           
-        allowedDevOrigins: ["http://localhost:3000", "http://127.0.0.1:3000"],
-        reactStrictMode: true,
+        
       
      
         async redirects() {
